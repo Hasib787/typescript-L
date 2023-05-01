acfd517a281c3e58e5466744e2fee73607e43803
Md. Masud Rana <mr.hasib45@gmail.com>
2023-05-01 15:31:44 +0600

    feat: :sparkles: practice primitive type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-const course: string ="I'm learning Typescript";
+//primitive type
+//string
+const course: string = "I'm learning Typescript";
 console.log(course);
+
+//number
+let age: number = 27;
+age.toString();
+console.log(age);
+//boolean
+const isMonday: boolean = true;
+console.log(isMonday);
+
+//null
+let phoneNumber: null;
+
+//undefine
+let userName: undefined;
